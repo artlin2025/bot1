19ab63f6072102779e23ff869998c24a3171c7eb
artlin2025 <ArtLin2025@gmail.com>
2025-02-04 09:09:24 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -62,3 +62,14 @@ def contact_support(message):
 
 # اجرای ربات
 bot.polling(none_stop=True)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
